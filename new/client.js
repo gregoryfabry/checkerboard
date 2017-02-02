337b470cb7e3ea4c9719aae0642e2a6018e3f193
Gregory Fabry <glfabry2@illinois.edu>
2017-02-01 18:23:56 -0600

    buttery.

--- new/client.js
+++ new/client.js
@@ -8,11 +8,8 @@
   };
 
   function Connection(ws, callback) {
-    for (var p in this.defaults)
-      this[p] = this.defaults[p];
-
-    this.observers = [];
-    this.actions = {};
+    for (var p in Connection.defaults)
+      this[p] = Connection.defaults[p];
 
     this.sendSeqToTimeoutId = {};
     this.sendSeq = 0;
@@ -20,16 +17,84 @@
     this.receiveQueue = [];
     this.receiveSeq = 0;
 
+    this.transactionQueue = {};
+    this.transactionSeq = 0;
+
+    this.store = {};
+    this.observers = [];
+
     this.ws = new WebSocket(ws);
     this.ws.addEventListener("message", this.receive.bind(this));
     this.ws.addEventListener("open", callback.bind(this));
   }
 
-  Connection.prototype.defaults = {
+  Connection.defaults = {
     timeout: 1000
   };
 
-  
+  Connection.prototype.sync = function(id) {
+    this.storeId = id;
+    this.send("sync", id);
+  };
+
+  Connection.prototype.addObserver = function(observer) {
+    this.observers.push(observer);
+  };
+
+  Connection.prototype.removeObserver = function(observer) {
+    this.observers.splice(this.observers.indexOf(observer), 1);
+  };
+
+  Connection.prototype.transaction = function(paths, action, seq) {
+    seq = (typeof seq !== "undefined" ? seq : this.transactionSeq++);
+
+    paths = paths.map((function(path) {
+      if (typeof path === "string" || path instanceof String)
+        return path.split(".");
+
+      return path;
+    }).bind(this));
+
+    var dependencies = paths.map((function(path) {
+      return getByPath(this.store, path);
+    }).bind(this));
+
+    action.apply(null, dependencies);
+
+    var versions = {};
+    var updates = {};
+    for (var i = 0; i < paths.length; i++) {
+      var version = dependencies[i]._id || 0;
+      versions[paths[i].join(".")] = version;
+      dependencies[i]._id = version + 1;
+      updates[paths[i].join(".")] = dependencies[i];
+    }
+
+    this.transactionQueue[seq] = {paths: paths, action: action};
+
+    this.send("transaction", {
+      seq: seq,
+      storeId: this.storeId,
+      versions: versions,
+      updates: updates
+    });
+
+    this.observers.forEach((function(observer) {
+      observer(this.store);
+    }).bind(this));
+  };
+
+  function getByPath(obj, path) {
+    if (path.length === 0)
+      return obj;
+
+    if (!(path[0] in obj))
+      obj[path[0]] = {};
+
+    var next = obj[path[0]];
+
+    return getByPath(next, path.slice(1));
+  }
 
   // Sending and receiving logic.
   Connection.prototype.send = function(channel, message, seq) {
@@ -57,7 +122,7 @@
 
       while (this.receiveQueue.length > 0 && this.receiveSeq === this.receiveQueue[0].seq) {
         this.receiveSeq++;
-        this.processReceive(this.receiveQueue.splice(0, 1));
+        this.processReceive(this.receiveQueue.shift());
       }
     }
   };
@@ -66,9 +131,40 @@
     var channel = envelope.channel,
         message = envelope.message;
 
+    var p, path;
     switch(channel) {
-      case "set-state":
-        this.state = message;
+      case "set-store":
+        if (message.storeId !== this.storeId)
+          break;
+
+        this.store = message.store;
+
+        this.observers.forEach((function(observer) {
+          observer(this.store);
+        }).bind(this));
+
+        break;
+      case "updates":
+        if (message.storeId !== this.storeId)
+          break;
+
+        for (p in message.updates) {
+          path = p.split(".");
+          getByPath(this.store, path.slice(0, -1))[path.pop()] = message.updates[p];
+        }
+
+        this.observers.forEach((function(observer) {
+          observer(this.store);
+        }).bind(this));
+
+        break;
+      case "transaction-success":
+        delete this.transactionQueue[message.seq];
+        break;
+      case "transaction-failure":
+        var transaction = this.transactionQueue[message.seq];
+        delete this.transactionQueue[message.seq];
+        this.transaction(transaction.paths, transaction.action, message.seq);
         break;
     }
   };
--- new/server.js
+++ new/server.js
@@ -11,25 +11,103 @@ function Server(port, dir) {
   this.dir = dir;
   this.connections = [];
 
+  this.stores = {};
+  this.subscriptions = {};
+
   this.server.on("connection", (function(ws) {
     var connection = new Connection(ws);
 
     connection.on("message", this.processReceive.bind(this));
-
+    connection.on("close", (function() {
+      this.connections.splice(this.connections.indexOf(connection), 1);
+    }).bind(this));
     this.connections.push(connection);
   }).bind(this));
 
   EventEmitter.call(this);
 }
 
-Server.prototype.processReceive = function(envelope) {
+Server.prototype.processReceive = function(connection, envelope) {
   var channel = envelope.channel,
       message = envelope.message;
 
   switch(channel) {
-    
+    case "sync":
+      var id = message;
+
+      if (typeof connection.storeId !==  "undefined") {
+        var indexOfConnection = this.subscriptions[connection.storeId].indexOf(connection);
+        this.subscriptions[connection.storeId].splice(indexOfConnection, 1);
+      }
+
+      if (!(id in this.stores)) {
+        this.stores[id] = {};
+        this.subscriptions[id] = [];
+      }
+
+      this.subscriptions[id].push(connection);
+      connection.storeId = id;
+
+      connection.send("set-store", {
+        storeId: id,
+        store: this.stores[id]
+      });
+      break;
+    case "transaction":
+      var store = this.stores[connection.storeId];
+      if (connection.storeId !== message.storeId)
+        return;
+      var p, path;
+      for (p in message.versions) {
+        path = p.split(".");
+        if ((getByPath(store, path)._id || 0) !== message.versions[p]) {
+          var updates = {};
+          for (p in message.versions) {
+            path = p.split(".");
+            updates[p] = getByPath(store, path);
+            connection.send("transaction-failure", {
+              seq: message.seq,
+              updates: updates
+            });
+
+            return;
+          }
+        }
+      }
+
+      for (p in message.updates) {
+        path = p.split(".");
+        getByPath(store, path.slice(0, -1))[path.pop()] = message.updates[p];
+      }
+
+      this.subscriptions[message.storeId].forEach(function(subscription) {
+        if (subscription === connection)
+          return;
+
+        subscription.send("updates", {
+          storeId: message.storeId,
+          updates: message.updates
+        });
+      });
+
+      connection.send("transaction-success", {
+        seq: message.seq
+      });
+      break;
   }
 };
+
+function getByPath(obj, path) {
+  if (path.length === 0)
+    return obj;
+
+  if (!(path[0] in obj))
+    obj[path[0]] = {};
+
+  var next = obj[path[0]];
+
+  return getByPath(next, path.slice(1));
+}
 
 function Connection(ws) {
   for (var p in this.defaults)
@@ -57,7 +135,11 @@ Connection.prototype.defaults = {
 Connection.prototype.send = function(channel, message, seq) {
   seq = (typeof seq !== "undefined" ? seq : this.sendSeq++);
 
+  if (this.ws.readyState !== 1)
+    return;
+
   this.ws.send(JSON.stringify(new Envelope(channel, message, seq)));
+
   this.sendSeqToTimeoutId[seq] = setTimeout((function() {
     this.send(channel, message, seq);
   }).bind(this), this.timeout);
@@ -67,13 +149,10 @@ Connection.prototype.receive = function(json) {
   var data = JSON.parse(json);
   var envelope = new Envelope(data.channel, data.message, data.seq);
 
-  console.log(data);
-
   if (envelope.channel === "ack") {
     clearTimeout(this.sendSeqToTimeoutId[envelope.seq]);
     delete this.sendSeqToTimeoutId[envelope.seq];
   } else if (envelope.seq >= this.receiveSeq) {
-    console.log("receive");
     this.ws.send(JSON.stringify(new Ack(envelope.seq)));
 
     this.receiveQueue.push(envelope);
@@ -83,7 +162,7 @@ Connection.prototype.receive = function(json) {
 
     while (this.receiveQueue.length > 0 && this.receiveSeq === this.receiveQueue[0].seq) {
       this.receiveSeq++;
-      this.emit("message", this.receiveQueue.splice(0, 1));
+      this.emit("message", this, this.receiveQueue.shift());
     }
   }
 };
